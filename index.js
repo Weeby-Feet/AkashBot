daefbcc705539d850662c18f8a19b5ad18e2fd76
Nathan Ellison <nathan.ipadman@gmail.com>
2019-09-11 14:27:24 +1200

    AkashBot is now able to successfully connect to the main voice channel of my discord server

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'NjIxMTUwMzk3MjA1MzE1NTg2.XXhKHQ.rO_VFTBLW7a0vS6P_2qOhBsl5nk';
+const token = 'NjIxMTUwMzk3MjA1MzE1NTg2.XXhT9w.liFoevq7VZE_SzNossFrQI5pkNE';
 
 bot.on('ready', () => {
     console.log('AkashBot is now online!');
-})
 
-bot.on('message', msg=> {
-    if(msg.content === "Who is fat?") {
-        msg.reply('Akash is fat');
-    }
-})
+    const channel = bot.channels.get("346961010583666691");
+    if(!channel) return console.error("This channel does not exit blin!");
+    channel.join().then(connection => {
+      //It worked!
+      console.log("Successfully connected");
+    }).catch(e => {
+      //Error!
+      console.error(e);
+    });
+});
 
-bot.on('message', msg=> {
-    if(msg.content === "Who has Gilbert Syndrome?") {
-        msg.reply('Akash has Gilbert Syndrome');
+const PREFIX = '&'
+
+bot.on('message', message=> {
+
+    let args = message.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]) {
+      case 'ping':
+        message.channel.send('pong');
     }
+
 })
 
 bot.login(token);
