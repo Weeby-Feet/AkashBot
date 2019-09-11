548d073b8e0a482b94cd149332acf1dd3d35b76d
Nathan Ellison <nathan.ipadman@gmail.com>
2019-09-11 16:23:15 +1200

    AkashBot can now mention a specific user and play audio files in the voice channel of the user that summoned it

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
 const Discord = require('discord.js');
+const token = require('./token.js').token;
 const bot = new Discord.Client();
 
-const token = 'NjIxMTUwMzk3MjA1MzE1NTg2.XXhT9w.liFoevq7VZE_SzNossFrQI5pkNE';
+var channel = '';
 
 bot.on('ready', () => {
     console.log('AkashBot is now online!');
-
-    const channel = bot.channels.get("346961010583666691");
-    if(!channel) return console.error("This channel does not exit blin!");
-    channel.join().then(connection => {
-      //It worked!
-      console.log("Successfully connected");
-    }).catch(e => {
-      //Error!
-      console.error(e);
-    });
 });
 
+//Server members need to type this character beore their command for the bot to notice it
 const PREFIX = '&'
 
+//The bot listens for a message to be sent to any channel it can see
 bot.on('message', message=> {
+
+    if(message.content === "who is fat") {
+      message.channel.send('<@267946818254405642> is fat');
+    }
 
     let args = message.content.substring(PREFIX.length).split(" ");
 
     switch(args[0]) {
       case 'ping':
         message.channel.send('pong');
+        break;
+      case 'SNA':
+        console.log("Playing Soviet National Anthem");
+
+        //Only true slav will understand
+        message.channel.send("Да товарищ");
+
+        channel = message.member.voiceChannel;
+        console.log("Joining channel " + channel.name);
+
+        channel.join().then(connection => {
+          const dispatcher = connection.playFile('National Anthem of USSR.mp3');
+
+          //When the song ends, leave the channel
+          dispatcher.on("end", end => {
+            channel.leave();
+          });
+        });
+        break;
+
+      case 'leave':
+        //Say bye bye to server and developer. Good bot.
+        console.log("Bye!");
+        message.channel.send("Bye!");
+        channel.leave();
     }
 
 })
