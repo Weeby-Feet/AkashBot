c79a5baf268ba2c4f74a79bc491c139a38c3fe29
Nathan Ellison <nathan.ipadman@gmail.com>
2019-09-18 20:53:11 +1200

    Added extra command to make bot leave voice channel, and the bot also replies to the user

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const token = require('./token.js').token;
 const bot = new Discord.Client();
 
 const ytdl = require('ytdl-core');
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube("AIzaSyDGh0OAIeql8rYKlS6w82FbrfxtLk7atYU");
 const queue = new Map();
 
 var channel = '';
@@ -19,10 +21,11 @@ bot.on('message', message=> {
 
     var serverQueue;
 
+    //If the message is from the bot itself or does not use the prefix, don't do anything
     if(message.author.bot) return;
     if(!message.content.startsWith(PREFIX)) return;
 
-    //The bot can only be communicated with in a server
+    //The bot can only be communicated with in a server, and not via direct message
     if(message.guild) {
       serverQueue = queue.get(message.guild.id);
     }
@@ -30,25 +33,29 @@ bot.on('message', message=> {
       return;
     }
 
-    //Music function commands
+    //Get the bot to play a song from youtube
     if(message.content.startsWith(`${PREFIX}play`)) {
       console.log("Playing");
       execute(message, serverQueue);
       return;
     }
+    //Skip the song that the bot is currently playing
     else if(message.content.startsWith(`${PREFIX}skip`)) {
       console.log("Skipping");
       skip(message, serverQueue);
       return;
     }
+    //Make the bot stop playing whatever it's playing
     else if(message.content.startsWith(`${PREFIX}stop`)) {
       console.log("Stopping");
       stop(message, serverQueue);
       return;
     }
+    //Ping the bot to check it's latency
     else if(message.content.startsWith(`${PREFIX}ping`)) {
       message.channel.send('Pong! My ping is ' + bot.ping + "ms");
     }
+    //Make the bot join the same voice channel as the person that summoned it
     else if(message.content.startsWith(`${PREFIX}join`)) {
       //Check that the user that summoned the bot is actually in a voice channel
       if(!message.member || !message.member.voiceChannel) {
@@ -62,6 +69,7 @@ bot.on('message', message=> {
         //The company is enough of a function :)
       });
     }
+    //You'll find out what this does eventually...
     else if(message.content.startsWith(`${PREFIX}SNA`)) {
       //Check that the user that summoned the bot is actually in a voice channel
       if(!message.member || !message.member.voiceChannel) {
@@ -83,6 +91,7 @@ bot.on('message', message=> {
         });
       });
     }
+    //Make the bot leave the voice channel
     else if(message.content.startsWith(`${PREFIX}leave`)) {
       if(!message.member || !message.member.voiceChannel) {
         return;
@@ -92,18 +101,37 @@ bot.on('message', message=> {
       message.channel.send("Bye!");
       message.member.voiceChannel.leave();
     }
+    //For those that are so inclined to use such vulgar language
     else if(message.content.startsWith(`${PREFIX}fuckoff`)) {
       if(!message.member || !message.member.voiceChannel) {
         message.reply('Join a voice channel before telling me to do that m8');
       }
 
       console.log("Fucking off");
-      message.channel.send("Fuck you too " + message.member);
+
+      //You can change the max value depending on how many repsonses you want
+      var min = 0;
+      var max = 2;
+
+      //Randomly choose a response
+      var reply = Math.floor(Math.random() * (+max - +min)) + +min;
+
+      switch(reply) {
+        case 0:
+          message.channel.send("Mind your ruddy language, my ears are not a toilet " + message.member);
+          break;
+        case 1:
+          message.channel.send("Fuck you too " + message.member);
+          break;
+      }
+
       message.member.voiceChannel.leave();
     }
+    //We all know who's fat
     else if(message.content.startsWith(`${PREFIX}whoisfat`)) {
       message.channel.send('<@267946818254405642> is fat');
     }
+    //Get help
     else if(message.content.startsWith(`${PREFIX}help`)) {
       console.log(message.author.username + " requested help");
       message.channel.send("Sent you a DM with my commands");
@@ -118,8 +146,10 @@ async function execute(message, serverQueue) {
     console.log("Member needs to be in a voice channel first");
   }
 
+  //Split up the command so it can be analysed
   const args = message.content.split(' ');
 
+  //If the user is not in a voice channel, or the bot doesn't hav permission to join, alert the user
   const voiceChannel = message.member.voiceChannel;
   if(!voiceChannel) return message.channel.send('You need to be in a voice channel for me to sing to you');
   const permissions = voiceChannel.permissionsFor(message.client.user);
@@ -167,7 +197,7 @@ async function execute(message, serverQueue) {
   }
 }
 
-//Skip a song that is currently playing
+//Skip the song that is currently playing
 function skip(message, serverQueue) {
   if(!message.member.voiceChannel) return message.channel.send("You have to be in a voice channel to do that");
   if(!serverQueue) return message.channel.send("There is no song that I could skip!");
